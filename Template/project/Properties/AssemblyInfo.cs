2a67fdea5e21e5ea21d43738c1c402c095a3a759
cafeunder <cafe3641@gmail.com>
2016-08-06 02:56:41 +0900

    は？

--- Template/project/Properties/AssemblyInfo.cs
+++ Template/project/Properties/AssemblyInfo.cs
@@ -5,7 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は、以下の属性セットによって 
 // 制御されます。 アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
+[assembly: AssemblyTitle("webProject")]
+[assembly: AssemblyDescription("webクライアント開発のためのソリューション")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("webProject")]
+[assembly: AssemblyCopyright("Copyright © cafeunder 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
